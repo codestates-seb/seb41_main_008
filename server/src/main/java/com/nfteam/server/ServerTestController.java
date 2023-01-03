672b6f2b8c50b3034ee524bae29ed80147447be1
sojeonglee <sojeonglee0125@gmail.com>
2023-01-03 20:29:05 +0900

    feat: BE 프로젝트 초기 환경 설정

---
+++ server/src/main/java/com/nfteam/server/ServerTestController.java
@@ -0,0 +1,13 @@
+package com.nfteam.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerTestController {
+
+    @GetMapping
+    public String homeTest() {
+        return "서버 정상 동작 확인";
+    }
+}
