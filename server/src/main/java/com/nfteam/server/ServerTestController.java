95ea9a97f07d3f09fb563bda4fb73be15ebd1bfd
sojeonglee <sojeonglee0125@gmail.com>
2023-01-03 23:24:24 +0900

    test: 슬랙 연동 테스트

--- server/src/main/java/com/nfteam/server/ServerTestController.java
+++ server/src/main/java/com/nfteam/server/ServerTestController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ServerTestController {
 
+    //
     @GetMapping
     public String homeTest() {
         return "서버 정상 동작 확인";
