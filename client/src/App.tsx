6e3b702934004969dfc24704a99a4521f4329dca
Valentin1495 <newface95@icloud.com>
2023-01-04 16:46:12 +0900

    FE/feat: carousel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
+import Carousel from 'components/Carousel';
 import Header from 'components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Carousel />
     </div>
   );
 }
--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import DropdownItems from './DropdownItems';
+
 const Header = () => {
   const SearchInput = styled.input`
     display: flex;
