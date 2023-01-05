3967149da11bc181bb168a7ea1d3a4cffdecee4a
sojeonglee <sojeonglee0125@gmail.com>
2023-01-05 14:45:41 +0900

    feat: coin 관련 엔티티 설계 1차 완성

---
+++ server/src/main/java/com/nfteam/server/coin/Coin.java
@@ -0,0 +1,38 @@
+package com.nfteam.server.coin;
+
+import com.nfteam.server.item.entity.ItemPrice;
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name = "coin")
+public class Coin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "coin_id")
+    private Long coinId;
+
+    @Column(name = "coin_name", nullable = false, length = 100)
+    private String coinName;
+
+    @Column(name = "current_price", nullable = false, length = 400)
+    private Long currentPrice;
+
+    @OneToMany(mappedBy = "coin")
+    private List<ItemPrice> itemPrices = new ArrayList<>();
+
+    protected Coin() {
+    }
+
+    @Builder
+    public Coin(String coinName, Long currentPrice) {
+        this.coinName = coinName;
+        this.currentPrice = currentPrice;
+    }
+}
