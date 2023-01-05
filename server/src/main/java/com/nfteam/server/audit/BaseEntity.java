012b40bffe7d07abefbe8e65090c7c0adbaca7a1
sojeonglee <sojeonglee0125@gmail.com>
2023-01-05 14:45:02 +0900

    refactor: Audit BaseEntity 주석 제거

--- server/src/main/java/com/nfteam/server/audit/BaseEntity.java
+++ server/src/main/java/com/nfteam/server/audit/BaseEntity.java
@@ -23,6 +23,4 @@ public class BaseEntity {
     @Column(name = "modified_date", nullable = false)
     private LocalDateTime modifiedDate;
 
-    // 생성한 사람을 등록하고 수정한 사람를 자동으로 등록하는 부분은 스프링 시큐리티 컨텍스트에서 연동해서 가져와야함
-    // AuditorAware 적용이 필요해서 구현해야 하고, 더불어 현재 애플리케이션에서는 이미 유저 정보가 다 연관되어 있어서 불필요한 정보라서 제외
 }
