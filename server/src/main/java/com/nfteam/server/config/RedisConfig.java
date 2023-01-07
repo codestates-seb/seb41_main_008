31ae754597d1fef2bae782bb8c2378c53778c962
sojeonglee <sojeonglee0125@gmail.com>
2023-01-07 15:08:20 +0900

    feat: 레디스 설정파일에 비밀번호 설정 추가

--- server/src/main/java/com/nfteam/server/config/RedisConfig.java
+++ server/src/main/java/com/nfteam/server/config/RedisConfig.java
@@ -2,27 +2,36 @@ package com.nfteam.server.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
-@Configuration
 @EnableRedisRepositories
+@Configuration
 public class RedisConfig {
-    //@Value("${spring.redis.host}")
-    private String host = "ec2-43-200-106-60.ap-northeast-2.compute.amazonaws.com";
-    //@Value("${spring.redis.port}")
-    private int port = 6379;
+
+    @Value("${spring.redis.host}")
+    private String host;
+    @Value("${spring.redis.port}")
+    private int port;
+    @Value("${spring.redis.password}")
+    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host,port);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        return lettuceConnectionFactory;
     }
 
     @Bean
@@ -33,4 +42,6 @@ public class RedisConfig {
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         return redisTemplate;
     }
+
+
 }
