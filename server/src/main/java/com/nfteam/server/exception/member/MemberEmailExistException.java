2153ed494dd922969e5b39178b61ea8946fad899
sojeonglee <sojeonglee0125@gmail.com>
2023-01-08 15:56:27 +0900

    feat: 예외 처리 클래스 추가

---
+++ server/src/main/java/com/nfteam/server/exception/member/MemberEmailExistException.java
@@ -0,0 +1,13 @@
+package com.nfteam.server.exception.member;
+
+import com.nfteam.server.exception.ExceptionCode;
+import com.nfteam.server.exception.NFTCustomException;
+
+public class MemberEmailExistException extends NFTCustomException {
+
+    private static final String message = "해당 이메일을 가진 회원이 이미 존재합니다.";
+
+    public MemberEmailExistException(String email) {
+        super(ExceptionCode.MEMBER_EMAIL_EXIST, String.format("%s - 중복된 이메일 : %s", message, email));
+    }
+}
