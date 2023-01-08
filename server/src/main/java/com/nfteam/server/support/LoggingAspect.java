44b7c48f90f6415eb4cfbb90a6d0995d88b9345f
sojeonglee <sojeonglee0125@gmail.com>
2023-01-08 16:26:09 +0900

    feat: Request 발생 시 request 기록하는 로깅 aop 추가 및 jpaConfig 설정 독립 클래스 작성

---
+++ server/src/main/java/com/nfteam/server/support/LoggingAspect.java
@@ -0,0 +1,56 @@
+package com.nfteam.server.support;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.CodeSignature;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Pointcut("execution(* com.nfteam.server.*.controller.*Controller.*(..))")
+    private void controllerPointcut() {
+    }
+
+    @Pointcut("execution(* com.nfteam.server.*.service.*Service.*(..))")
+    private void servicePointcut() {
+    }
+
+    @Before("controllerPointcut()")
+    public void requestLog(JoinPoint joinPoint) throws JsonProcessingException {
+        CodeSignature signature = (CodeSignature) joinPoint.getSignature();
+        Map<String, Object> paramMap = extractParameters(joinPoint, signature);
+
+        try {
+            log.info("============= [REQUEST] Controller: {},  Method: {}, Arguments: {} =============",
+                    joinPoint.getTarget().getClass().getSimpleName(), signature.getName(),
+                    objectMapper.writeValueAsString(paramMap));
+        } catch (JsonProcessingException e) {
+            log.warn("logging error");
+        }
+    }
+
+    private Map<String, Object> extractParameters(JoinPoint joinPoint, CodeSignature signature) {
+        String[] parameterNames = signature.getParameterNames();
+        Map<String, Object> paramMap = new HashMap<>();
+
+        int i = 0;
+        for (String parameterName : parameterNames) {
+            paramMap.put(parameterName, joinPoint.getArgs()[i++]);
+        }
+
+        return paramMap;
+    }
+}
