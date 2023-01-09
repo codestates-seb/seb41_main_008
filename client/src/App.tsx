a9240bf745a6afdd62fd7e8154e392d7a357ca22
taejinii <xowls177@gmail.com>
2023-01-09 15:50:37 +0900

    feat:헤더바 및 로그인,회원가입페이지 css수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,25 @@
-import Footer from './components/Layout/Footer';
-import Header from './components/header/Header';
-import Carousel from './components/Carousel';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LoginPage from '../src/pages/LoginPage';
+import SignupPage from '../src/pages/SignupPage';
+import MainPage from '../src/pages/MainPage';
+import MainLayout from '../src/components/Layout/MainLayout';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Footer />
-      <Carousel />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<MainPage />} />
+          </Route>
+
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
+/**MainLayout 컴포넌트에는 전역에 보여주고싶은 컴포넌트들이 들어있다 ex)header,footer,cartModal */
+/**MainLayout 에 감싸지는 페이지들은 MainLayout이 가지고있는 컴포넌트들을 다 보여주며 밖에있는 로그인,회원가입페이지는 header,footer등이 안보이게된다. */
 export default App;
--- client/src/components/header/DropdownItems.tsx
+++ client/src/components/header/DropdownItems.tsx
@@ -1,7 +1,7 @@
 const DropdownItems = () => {
   return (
     <div className="absolute ">
-      <ul className="w-48 shadow-2xl rounded-xl bg-white">
+      <ul className="w-40 shadow-2xl rounded-xl bg-white">
         <li className="rounded-t-xl py-4 shadow hover:border-b-2">TEST</li>
         <li className="py-4 shadow hover:border-y-2">TEST</li>
         <li className="py-4 shadow hover:border-y-2">TEST</li>
