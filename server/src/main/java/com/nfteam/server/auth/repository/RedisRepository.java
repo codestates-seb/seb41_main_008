f89c7bfead3d41fc03473a772ea90f84f3fd9986
sojeonglee <sojeonglee0125@gmail.com>
2023-01-09 09:31:54 +0900

    feat: 1차 코드 합병

--- server/src/main/java/com/nfteam/server/auth/repository/RedisRepository.java
+++ server/src/main/java/com/nfteam/server/auth/repository/RedisRepository.java
@@ -1,23 +1,17 @@
 package com.nfteam.server.auth.repository;
 
 import com.nfteam.server.auth.jwt.JwtTokenizer;
-
-import java.util.concurrent.TimeUnit;
-
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Repository;
 
+import java.util.concurrent.TimeUnit;
+
 @Repository
 @RequiredArgsConstructor
 public class RedisRepository {
-
-    // 필드 인젝션은 안좋습니다. 그리고 여기서 필요가 없습니당
-    @Autowired
     private final RedisTemplate<String, String> redisTemplate;
-
     private final JwtTokenizer jwtTokenizer;
 
     public void saveRefresh(String refreshToken, String value) {
