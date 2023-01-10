f09104aa1768a47773e61d0228282e5d23dd6653
kongalabear <kongalabear@gmail.com>
2023-01-10 14:39:46 +0900

    test

--- server/src/main/java/com/nfteam/server/ServerTestController.java
+++ server/src/main/java/com/nfteam/server/ServerTestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServerTestController {
     @GetMapping
     public String homeTest() {
+
         return "운영서버 무중단 배포 확인 2";
-    }
+   }
 }
