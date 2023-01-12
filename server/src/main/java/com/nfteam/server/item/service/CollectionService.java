c2b1e4a02a73e3cf6d2c5b916ea0c10477d2dae1
sojeonglee <sojeonglee0125@gmail.com>
2023-01-12 22:43:23 +0900

    feat: 시큐리티 권한 저장문제로 아이디 대신 이메일로 체크 방식 변경

--- server/src/main/java/com/nfteam/server/item/service/CollectionService.java
+++ server/src/main/java/com/nfteam/server/item/service/CollectionService.java
@@ -9,11 +9,13 @@ import com.nfteam.server.dto.response.item.ItemResponseDto;
 import com.nfteam.server.dto.response.item.UserCollectionResponse;
 import com.nfteam.server.exception.auth.NotAuthorizedException;
 import com.nfteam.server.exception.item.ItemCollectionNotFoundException;
+import com.nfteam.server.exception.member.MemberNotFoundException;
 import com.nfteam.server.item.entity.Item;
 import com.nfteam.server.item.entity.ItemCollection;
 import com.nfteam.server.item.repository.CollectionRepository;
 import com.nfteam.server.item.repository.ItemRepository;
 import com.nfteam.server.member.entity.Member;
+import com.nfteam.server.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,20 +30,27 @@ public class CollectionService {
 
     private final CollectionRepository collectionRepository;
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     @Transactional
     public Long save(CollectionCreateRequest request, MemberDetails memberDetails) {
         ItemCollection itemCollection = request.toCollection();
-        itemCollection.assignMember(new Member(memberDetails.getMemberId()));
+        Member member = findMember(memberDetails.getEmail());
+        itemCollection.assignMember(member);
         itemCollection.assignCoin(new Coin(Long.parseLong(request.getCoinId())));
 
         return collectionRepository.save(itemCollection).getCollectionId();
     }
 
+    private Member findMember(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new MemberNotFoundException(email));
+    }
+
     @Transactional
     public Long update(Long collectionId, CollectionPatchRequest request, MemberDetails memberDetails) {
         ItemCollection itemCollection = getCollectionById(collectionId);
-        checkValidAuth(itemCollection.getMember().getMemberId(), memberDetails.getMemberId());
+        checkValidAuth(itemCollection.getMember().getEmail(), memberDetails.getEmail());
         itemCollection.update(request.toCollection());
 
         return itemCollection.getCollectionId();
@@ -52,8 +61,8 @@ public class CollectionService {
                 .orElseThrow(() -> new ItemCollectionNotFoundException(collectionId));
     }
 
-    private void checkValidAuth(Long colMemberId, long authMemberId) {
-        if (colMemberId != authMemberId) {
+    private void checkValidAuth(String savedEmail, String authEmail) {
+        if (!savedEmail.equals(authEmail)) {
             throw new NotAuthorizedException();
         }
     }
@@ -61,7 +70,7 @@ public class CollectionService {
     @Transactional
     public Long delete(Long collectionId, MemberDetails memberDetails) {
         ItemCollection itemCollection = getCollectionById(collectionId);
-        checkValidAuth(itemCollection.getMember().getMemberId(), memberDetails.getMemberId());
+        checkValidAuth(itemCollection.getMember().getEmail(), memberDetails.getEmail());
         collectionRepository.deleteById(collectionId);
 
         return itemCollection.getCollectionId();
