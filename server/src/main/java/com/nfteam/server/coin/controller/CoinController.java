8fe47e61ae0d6d2263aaa8cafdeb7552d3f6e543
ShinHB417 <heebum417@naver.com>
2023-01-12 18:13:14 +0900

    Feat : openfeign사용해 DB에 수수료정보 저장하기 by @Scheduled

--- server/src/main/java/com/nfteam/server/coin/controller/CoinController.java
+++ server/src/main/java/com/nfteam/server/coin/controller/CoinController.java
@@ -4,8 +4,6 @@ import com.nfteam.server.coin.service.CoinService;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,12 +20,6 @@ public class CoinController {
 
     private final List<String> COIN = List.of("SOL","BTC","DOGE","ETH","ETC");
 
-
-//    @GetMapping("/price")
-//    public ResponseEntity getPrice(){
-//        coinService.getPrice(COIN);
-//        return new ResponseEntity("최근거래가 업데이트 완료.", HttpStatus.OK);
-//    }
 
     @Scheduled(cron = "0 0 4 25 * *")
     @GetMapping("/withdrawl-fee")
