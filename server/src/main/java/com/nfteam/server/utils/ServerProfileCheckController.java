380cc88bc7257fc5c7bba86af083335910de11db
sojeonglee <sojeonglee0125@gmail.com>
2023-01-13 08:55:45 +0900

    fix: 리다이렉트 url 카카오 오류 수정

--- server/src/main/java/com/nfteam/server/utils/ServerProfileCheckController.java
+++ server/src/main/java/com/nfteam/server/utils/ServerProfileCheckController.java
@@ -28,6 +28,6 @@ public class ServerProfileCheckController {
 
     @GetMapping
     public String homeTest() {
-        return "개발 서버 정상 동작 확인 테스트 - 시큐리티 / Oauth 리다이렉트 변경 마지막 체크22";
+        return "개발 서버 정상 동작 확인 테스트 - 시큐리티 / Oauth 리다이렉트 변경 마지막 체크 333";
     }
 }
