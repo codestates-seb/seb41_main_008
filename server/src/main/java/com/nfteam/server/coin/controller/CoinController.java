0b8113c11216a4fc42f4a9a330a1cc9c72c02b4d
ShinHB417 <heebum417@naver.com>
2023-01-12 17:43:26 +0900

    Feat : openfeign사용해 DB에 수수료정보 저장하기 by @Scheduled

--- server/src/main/java/com/nfteam/server/coin/controller/CoinController.java
+++ server/src/main/java/com/nfteam/server/coin/controller/CoinController.java
@@ -2,17 +2,20 @@ package com.nfteam.server.coin.controller;
 
 import com.nfteam.server.coin.service.CoinService;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//실시간 websocket 또는 SSE?
+
 @RestController
 @RequestMapping("/coins")
+@Slf4j
 public class CoinController {
     @Autowired
     private CoinService coinService;
@@ -26,11 +29,11 @@ public class CoinController {
 //        return new ResponseEntity("최근거래가 업데이트 완료.", HttpStatus.OK);
 //    }
 
-
+    @Scheduled(cron = "0 0 10 25 * *")
     @GetMapping("/withdrawl-fee")
-    public ResponseEntity getWithdrwalFee() throws Exception {
+    public void getWithdrwalFee() throws Exception {
         coinService.calculateFee(COIN);
-        return new ResponseEntity("거래수수료 업데이트 완료.", HttpStatus.OK);
+        log.info("withdrawlfee updated successfully!");
     }
 
 }
