c25fd30f37be9e6a30c5dde85455ba7bcd095f15
sojeonglee <sojeonglee0125@gmail.com>
2023-01-15 14:54:41 +0900

    feat: Oauth2 관련 api 리다이렉트 변경

--- server/src/main/java/com/nfteam/server/common/utils/ServerProfileCheckController.java
+++ server/src/main/java/com/nfteam/server/common/utils/ServerProfileCheckController.java
@@ -28,6 +28,6 @@ public class ServerProfileCheckController {
 
     @GetMapping
     public String homeTest() {
-        return "개발 서버 정상 동작 확인 테스트 - Oauth 리다이렉트 변경 변경사항 반영";
+        return "개발 서버 정상 동작 확인 테스트 - Oauth 변경사항 반영";
     }
 }
