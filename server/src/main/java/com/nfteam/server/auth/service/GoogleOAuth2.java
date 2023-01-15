c1645814f8efbb92b876a7244c6d5ff7ee4f449f
sojeonglee <sojeonglee0125@gmail.com>
2023-01-16 07:03:29 +0900

    feat: Oauth2 accessToken 받아서 접근하도록 변경

--- server/src/main/java/com/nfteam/server/auth/service/GoogleOAuth2.java
+++ server/src/main/java/com/nfteam/server/auth/service/GoogleOAuth2.java
@@ -83,6 +83,7 @@ public class GoogleOAuth2 implements OAuth2 {
         Member member = memberRepository.findByEmail(googleUser.getEmail())
                 .orElseThrow(() -> new MemberNotFoundException(googleUser.getEmail()));
         member.updateNickname(googleUser.getName());
+        member.updateLastLoginTime();
 
         MemberDetails memberDetails = new MemberDetails(member);
         String accessToken = "Bearer " + jwtTokenizer.generateAccessToken(memberDetails);
