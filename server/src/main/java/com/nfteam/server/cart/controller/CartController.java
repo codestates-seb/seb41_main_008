4c148a69cf8e0f16f8c5fb2ddde1c56fab65cf93
ShinHB417 <heebum417@naver.com>
2023-01-17 09:51:41 +0900

    feat : controller

---
+++ server/src/main/java/com/nfteam/server/cart/controller/CartController.java
@@ -0,0 +1,15 @@
+package com.nfteam.server.cart.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+
+}
