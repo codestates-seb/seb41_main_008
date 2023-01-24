9d9222cd965924494716444b2f1b6dee43fbfb7f
sojeonglee <sojeonglee0125@gmail.com>
2023-01-24 21:15:42 +0900

    refactor: Dto request 관련 정리 완료.

--- server/src/main/java/com/nfteam/server/dto/request/item/ItemSellRequest.java
+++ server/src/main/java/com/nfteam/server/dto/request/item/ItemSellRequest.java
@@ -4,12 +4,16 @@ import lombok.Getter;
 import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Getter
 public class ItemSellRequest {
 
     @NotNull(message = "가격은 필수값입니다.")
-    @Range(min = 0, max = 10000, message = "가격은 0 ~ 10000 이내로 입력해주세요.")
+    @Range(min = 0, max = 9999, message = "가격은 0 ~ 9999 이내로 입력해주세요.")
     private String itemPrice;
+
+    // todo: 판매 마감일 입력 받을 지 결정
+    private LocalDateTime saleEnds;
 
 }
