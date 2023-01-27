5a66165ac062093452ba49a4345d54d738b132ea
sojeonglee <sojeonglee0125@gmail.com>
2023-01-27 10:43:03 +0900

    refactor: exception 패키지 리팩토링 완료.

--- server/src/main/java/com/nfteam/server/exception/auth/NotAuthorizedException.java
+++ server/src/main/java/com/nfteam/server/exception/auth/NotAuthorizedException.java
@@ -5,7 +5,7 @@ import com.nfteam.server.exception.NFTCustomException;
 
 public class NotAuthorizedException extends NFTCustomException {
 
-    private static final String message = "권한이 필요합니다";
+    private static final String message = "해당 정보 접근에는 관리자 권한이 필요합니다";
 
     public NotAuthorizedException() {
         super(ExceptionCode.NOT_AUTHORIZED, message);
