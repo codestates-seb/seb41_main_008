d66651faf4bf67972439b168f964a76b1014dad5
Valentin1495 <newface95@icloud.com>
2023-02-02 10:04:22 +0900

    feat: 아이템 검색 기능

--- client/src/components/Search/ColResults.tsx
+++ client/src/components/Search/ColResults.tsx
@@ -11,45 +11,49 @@ export default function ColResults({
   cols: SearchCol[] | undefined;
 }) {
   return (
-    <div className="mt-20 p-[2rem] space-y-2">
-      <h5 className="font-bold">{cols?.length} collections</h5>
-      <Swiper
-        spaceBetween={15}
-        loop={true}
-        loopFillGroupWithBlank={true}
-        navigation={true}
-        modules={[Navigation]}
-        breakpoints={{
-          0: {
-            slidesPerView: 1,
-            slidesPerGroup: 1,
-          },
-          600: {
-            slidesPerView: 2,
-            slidesPerGroup: 2,
-          },
-          768: {
-            slidesPerView: 3,
-            slidesPerGroup: 3,
-          },
-          1024: {
-            slidesPerView: 4,
-            slidesPerGroup: 4,
-          },
-        }}
-        className="main-carousel rounded-2xl overflow-hidden"
-      >
-        {cols?.map((col) => (
-          <SwiperSlide className="group rounded-2xl" key={col.collectionId}>
-            <ColResult
-              collectionId={col.collectionId}
-              logoImgName={col.logoImgName}
-              bannerImgName={col.bannerImgName}
-              collectionName={col.collectionName}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+    <div className="mt-5 space-y-2">
+      <h5 className="font-bold text-lg ml-3.5">{cols?.length} collections</h5>
+      {cols?.length ? (
+        <Swiper
+          spaceBetween={15}
+          loop={true}
+          loopFillGroupWithBlank={true}
+          navigation={true}
+          modules={[Navigation]}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              slidesPerGroup: 1,
+            },
+            600: {
+              slidesPerView: 2,
+              slidesPerGroup: 2,
+            },
+            768: {
+              slidesPerView: 3,
+              slidesPerGroup: 3,
+            },
+            1024: {
+              slidesPerView: 4,
+              slidesPerGroup: 4,
+            },
+          }}
+          className="main-carousel rounded-2xl overflow-hidden"
+        >
+          {cols?.map((col) => (
+            <SwiperSlide className="group rounded-2xl" key={col.collectionId}>
+              <ColResult
+                collectionId={col.collectionId}
+                logoImgName={col.logoImgName}
+                bannerImgName={col.bannerImgName}
+                collectionName={col.collectionName}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <p className="ml-3.5">We couldn{"'"}t find any collections.</p>
+      )}
     </div>
   );
 }
--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -1,66 +1,102 @@
-/* eslint-disable */
 import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
-import { useQuery } from '@tanstack/react-query';
 import Header from 'components/Header/Header';
 import ColResults from 'components/Search/ColResults';
-import { SearchCol } from './MyCollection';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
+import ItemCard from 'components/ItemCard';
 
-interface itemsData {
-  itemPrice: number;
-  onSale: boolean;
-  itemId: number;
+export interface Item {
   itemImageName: string;
   itemName: string;
-  collectionId: number;
-  collectionName: string;
-  coinId: number;
+  itemPrice: number;
+  itemId: number;
   coinName: string;
   coinImage: string;
+  collectionName: string;
 }
 
-// interface pageData {
-//   totalElements: number;
-//   totalPages: number;
-//   size: number;
-//   page: number;
-// }
-
-interface Data {
-  data: itemsData[];
-}
-
-interface Results {
-  collections: SearchCol[];
-  items: Data;
-}
-
-const Search = () => {
+export default function Search() {
   const [searchParams] = useSearchParams();
   const query = searchParams.get('q');
 
-  const { isLoading, error, data } = useQuery<Results>({
-    queryKey: ['search', { keyword: query }],
-    queryFn: () =>
-      axios
-        .get(
-          `${process.env.REACT_APP_API_URL}/api/search?keyword=${query}&page=1&size=6`
-        )
-        .then((res) => res.data),
-  });
+  const { status, data, error, isFetchingNextPage, fetchNextPage } =
+    useInfiniteQuery(
+      ['search', { keyword: query }],
+      ({ pageParam = 1 }) =>
+        axios
+          .get(
+            `${process.env.REACT_APP_API_URL}/api/search?keyword=${query}&page=${pageParam}&size=12`
+          )
+          .then((res) => res.data),
 
-  console.log(data);
-  if (isLoading) return <p>Loading...</p>;
+      {
+        getNextPageParam: (lastPage, allPages) => {
+          return lastPage.length ? allPages.length + 1 : undefined;
+        },
+      }
+    );
+  const { ref, inView } = useInView();
 
-  if (error instanceof Error)
-    return <p> An error has occurred + {error.message}</p>;
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage]);
 
-  return (
-    <div>
+  return status === 'loading' ? (
+    <>
       <Header />
-      <ColResults cols={data?.collections} />
-    </div>
+      <p className="mt-20">Loading...</p>
+    </>
+  ) : status === 'error' && error instanceof Error ? (
+    <>
+      <Header />
+      <span className="mt-20">Error: {error.message}</span>
+    </>
+  ) : (
+    <>
+      <Header />
+      <div className="mt-32 px-8">
+        {data?.pages?.map((page, idx) => (
+          <>
+            <em className="text-lg">Results for {query}</em>
+            <ColResults cols={page.collections} />
+            {page.items.data.length ? (
+              <>
+                <h5 className="ml-3.5 mt-8 font-bold text-lg">
+                  {page.items.data.length} items
+                </h5>
+                <section
+                  key={idx}
+                  className="py-1.5 grid grid-cols-2 gap-y-3 gap-x-3 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
+                >
+                  {page.items.data.map((item: Item) => (
+                    <ItemCard
+                      key={item.itemId}
+                      itemImageName={
+                        process.env.REACT_APP_IMAGE + item.itemImageName
+                      }
+                      itemName={item.itemName}
+                      itemPrice={item.itemPrice}
+                      itemId={item.itemId}
+                      coinName={item.coinName}
+                      coinImage={item.coinImage}
+                      collectionName={item.collectionName}
+                    />
+                  ))}
+                </section>
+              </>
+            ) : (
+              <p>We coudln{"'"} find any items.</p>
+            )}
+          </>
+        ))}
+        <p className="inline-block" ref={ref}>
+          {isFetchingNextPage && 'Loading more...'}
+        </p>
+      </div>
+    </>
   );
-};
-
-export default Search;
+}
